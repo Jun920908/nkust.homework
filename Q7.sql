6946137572172c051987d8341d72068bfa77c5b9
Jun920908 <116427213+Jun920908@users.noreply.github.com>
2024-06-22 15:28:04 +0800

    Delete Q7.sql

--- Q7.sql
+++
@@ -1,8 +0,0 @@
-SELECT 'Male' AS Gender, 
-       (SELECT COUNT(*) FROM project.patient WHERE patient.Patient_sex = '男') AS PatientCount, 
-       (SELECT COUNT(*) FROM project.doctor WHERE doctor.Doctor_sex = '男') AS DoctorCount
-UNION 
-SELECT 'Female' AS Gender, 
-       (SELECT COUNT(*) FROM project.patient WHERE patient.Patient_sex = '女') AS PatientCount, 
-       (SELECT COUNT(*) FROM project.doctor WHERE doctor.Doctor_sex = '女') AS DoctorCount;
-       
