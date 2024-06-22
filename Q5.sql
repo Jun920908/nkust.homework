b175cb29e9c1c219128d98e50d6a97e2388ca739
Jun920908 <116427213+Jun920908@users.noreply.github.com>
2024-06-22 15:27:48 +0800

    Delete Q5.sql

--- Q5.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT doctor.Doctor_id,doctor.Doctor_name
-FROM project.doctor 
-WHERE doctor.Doctor_id NOT IN (
-    SELECT DISTINCT diagnose.Doctor_id
-    FROM project.diagnose
-    JOIN project.patient  ON diagnose.M_number=patient.M_number
-    WHERE patient.Patient_name = '王明宏'
-);
