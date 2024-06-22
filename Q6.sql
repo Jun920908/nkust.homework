337258d1a061604b7f04df625e7164f4b3cd1b4a
Jun920908 <116427213+Jun920908@users.noreply.github.com>
2024-06-22 15:27:56 +0800

    Delete Q6.sql

--- Q6.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT patient.M_number,patient.Patient_name
-FROM project.patient
-WHERE patient.M_number NOT IN (
-    SELECT DISTINCT diagnose.M_number
-    FROM project.diagnose
-    JOIN project.doctor  ON diagnose.Doctor_id=doctor.Doctor_id
-    WHERE doctor.Doctor_name = '李佳潔'
-);
