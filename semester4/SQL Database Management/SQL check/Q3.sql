00dbfd33114cd54be4e896063961efba21651322
Jun920908 <116427213+Jun920908@users.noreply.github.com>
2024-06-22 15:56:44 +0800

    Delete semester4/SQL Database Management/SQL check/Q3.sql

--- semester4/SQL Database Management/SQL check/Q3.sql
+++
@@ -1,7 +0,0 @@
-SELECT patient.Patient_name, COUNT(*) as often
-FROM project.patient
-JOIN project.diagnose ON diagnose.M_number=patient.M_number
-WHERE C_day="星期五"
-GROUP BY patient.Patient_name
-ORDER BY often DESC
-LIMIT 1;
